b4138729e049247f7a76aefd13afdf2c1d885aa2
pinjalu <pinjal.u@empiricinfotech.com>
2026-07-27 12:00:00 +0000

    fix(dao): store password digests salted and multi round (GL-6620)

--- sqli/dao/user.py
+++ sqli/dao/user.py
@@ -1,4 +1,6 @@
-from hashlib import md5
+from hashlib import pbkdf2_hmac
+from hmac import compare_digest
+from secrets import token_hex
 from typing import NamedTuple, Optional
 
 from aiopg import Connection
@@ -37,5 +39,27 @@ class User(NamedTuple):
             )
             return User.from_raw(await cur.fetchone())
 
+    @staticmethod
+    def hash_password(password: str):
+        salt = token_hex(16)
+        rounds = 200000
+        digest = pbkdf2_hmac('sha256', password.encode('utf-8'),
+                             bytes.fromhex(salt), rounds).hex()
+        return 'pbkdf2_sha256${}${}${}'.format(rounds, salt, digest)
+
+    @staticmethod
+    async def set_password(conn: Connection, id_: int, password: str):
+        async with conn.cursor() as cur:
+            await cur.execute('UPDATE users SET pwd_hash = %s WHERE id = %s',
+                              (User.hash_password(password), id_))
+
     def check_password(self, password: str):
-        return self.pwd_hash == md5(password.encode('utf-8')).hexdigest()
+        try:
+            scheme, rounds, salt, expected = self.pwd_hash.split('$')
+            if scheme != 'pbkdf2_sha256' or int(rounds) < 100000:
+                return False
+            actual = pbkdf2_hmac('sha256', password.encode('utf-8'),
+                                 bytes.fromhex(salt), int(rounds)).hex()
+            return compare_digest(actual, expected)
+        except (ValueError, TypeError):
+            return False
